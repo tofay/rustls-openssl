496bf2185a6ec323960dff5e172b4f5a50c72d3b
Tom Fay <tom@teamfay.co.uk>
2024-11-07 15:36:22 +0000

    give tests usable names

--- tests/it.rs
+++ tests/it.rs
@@ -185,52 +185,52 @@ fn test_with_custom_config_to_internet(
 }
 
 #[rstest]
-#[case(
+#[case::tls13_aes_128_gcm_sha256(
     TLS13_AES_128_GCM_SHA256,
     SECP384R1,
     CipherSuite::TLS13_AES_128_GCM_SHA256
 )]
-#[case(
+#[case::tls13_aes_256_gcm_sha384(
     TLS13_AES_256_GCM_SHA384,
     SECP256R1,
     CipherSuite::TLS13_AES_256_GCM_SHA384
 )]
 #[cfg_attr(
     feature = "chacha",
-    case(
+    case::TLS13_CHACHA20_POLY1305_SHA256(
         TLS13_CHACHA20_POLY1305_SHA256,
         SECP256R1,
         CipherSuite::TLS13_CHACHA20_POLY1305_SHA256
     )
 )]
-#[case(
+#[case::tls_ecdhe_rsa_with_aes_256_gcm_sha384(
     TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
     SECP256R1,
     CipherSuite::TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384
 )]
-#[case(
+#[case::tls_ecdhe_rsa_with_aes_128_gcm_sha256(
     TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
     SECP256R1,
     CipherSuite::TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256
 )]
 #[cfg_attr(
     feature = "x25519",
-    case(
+    case::TLS13_AES_256_GCM_SHA384(
         TLS13_AES_256_GCM_SHA384,
         rustls_openssl::X25519,
         CipherSuite::TLS13_AES_256_GCM_SHA384
     )
 )]
-#[case(
+#[case::tls13_aes_256_gcm_sha384(
     TLS13_AES_256_GCM_SHA384,
     SECP384R1,
     CipherSuite::TLS13_AES_256_GCM_SHA384
 )]
-// #[case(
-//     TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-//     SECP384R1,
-//     CipherSuite::TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384
-// )]
+#[case::tls_ecdhe_ecdsa_with_aes_256_gcm_sha384(
+    TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+    SECP384R1,
+    CipherSuite::TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384
+)]
 fn test_tls(
     #[case] suite: SupportedCipherSuite,
     #[case] group: &'static dyn SupportedKxGroup,
