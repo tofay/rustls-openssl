8b5fe2366b103ba1d8a0fdfb09178d9c5d51428e
Tom Fay <tom@teamfay.co.uk>
2024-11-11 22:08:16 +0000

    test using rustls provider tests

---
+++ tests/patch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rm -rf rustls/src/crypto/openssl
+cp -r ../rustls-openssl/src rustls/src/crypto/openssl
+mv rustls/src/crypto/openssl/lib.rs rustls/src/crypto/openssl/mod.rs
+find rustls/src/crypto/openssl/ -type f -exec sed -i 's/rustls::/crate::/g' {} \;
+git apply ../rustls-openssl/tests/0001-Patch-openssl-into-rustls.patch
