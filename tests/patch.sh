119f5844db38fa2601900e6d5628674847130b18
Tom Fay <tom@teamfay.co.uk>
2024-11-11 22:26:57 +0000

    another path

--- tests/patch.sh
+++ tests/patch.sh
@@ -4,4 +4,4 @@ rm -rf rustls/src/crypto/openssl
 cp -r ../rustls-openssl/src rustls/src/crypto/openssl
 mv rustls/src/crypto/openssl/lib.rs rustls/src/crypto/openssl/mod.rs
 find rustls/src/crypto/openssl/ -type f -exec sed -i 's/rustls::/crate::/g' {} \;
-git apply ../rustls_openssl/tests/0001-Patch-openssl-into-rustls.patch
+git apply ../rustls-openssl/tests/0001-Patch-openssl-into-rustls.patch
